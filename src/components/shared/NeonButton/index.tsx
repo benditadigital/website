5cd7d6fcb841e9ec8a44b8c7d18490bda5f72101
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-21 16:19:37 -0300

    :arrow_up: Update NeonButton

--- src/components/shared/NeonButton/index.tsx
+++ src/components/shared/NeonButton/index.tsx
@@ -2,16 +2,23 @@ import "./styles.css";
 
 interface NeonButtonProps {
   title: string;
+  icon?: JSX.Element;
+  className?: string;
 }
 
 function NeonButton(props: NeonButtonProps) {
   return (
-    <button className="neon-button font-poppins font-light px-5">
+    <button
+      className={"neon-button font-poppins font-light px-5 " + props.className}
+    >
       <span></span>
       <span></span>
       <span></span>
       <span></span>
-      <span className="text-white mx-3"> {props.title}</span>
+      <span className="text-white mx-3 flex justify-center items-center gap-3">
+        {" "}
+        {props.title} {props.icon}
+      </span>
     </button>
   );
 }
