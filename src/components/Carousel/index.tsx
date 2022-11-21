4b1afce2bc1c0f2ed1a9279470210acbc7d3a6b3
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-21 16:19:16 -0300

    :arrow_up: Update carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -7,131 +7,91 @@ import carousel4 from "../../assets/img/carousel/4.jpg";
 import { useEffect, useState } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
+import TinySlider from "tiny-slider-react";
+import "tiny-slider/dist/tiny-slider.css";
+
+import Slider from "react-slick";
+
 const slides = [
   {
+    index: "1",
     image: carousel1,
     title: "The Hand of 1",
     description: "The strongest STAND1.",
   },
   {
+    index: "2",
     image: carousel2,
     title: "The Hand of 2",
     description: "The strongest STAND2.",
   },
   {
+    index: "3",
     image: carousel3,
     title: "The Hand of 3",
     description: "The strongest STAND3.",
   },
   {
+    index: "4",
     image: carousel4,
     title: "The Hand of 4",
     description: "The strongest STAND4.",
   },
-  // repeat
-  {
-    image: carousel1,
-    title: "The Hand of 1",
-    description: "The strongest STAND5.",
-  },
-  {
-    image: carousel2,
-    title: "The Hand of 2",
-    description: "The strongest STAND6.",
-  },
-  {
-    image: carousel3,
-    title: "The Hand of 3",
-    description: "The strongest STAND7.",
-  },
-  {
-    image: carousel4,
-    title: "The Hand of 4",
-    description: "The strongest STAND8.",
-  },
 ];
 
-function Carousel() {
-  const [imagesOrder, setImagesOrder] = useState(["1", "2", "3"]);
-  const [nextImage, setNextImage] = useState("1");
-
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [usedButton, setUsedButton] = useState(false);
-
-  const [runAgain, setRunAgain] = useState(false);
-
-  useEffect(() => {
-    var timing = 3000;
-    if (!usedButton) {
-      setTimeout(function () {
-        if (currentSlide < 4) {
-          setCurrentSlide(currentSlide + 1);
-        } else {
-          setCurrentSlide(0);
-        }
-      }, timing);
-    } else {
-      console.log("Botão apertado! Ignorando");
-      setUsedButton(false);
-      setRunAgain(!runAgain);
-    }
-  }, [currentSlide, runAgain]);
-
-  var boxStyle = {
-    transform: `translateX(calc(var(--slide-width) * ${currentSlide} * -1))`,
-    transition: currentSlide === 0 ? "" : "all 1s ease 0s",
-  };
-
-  function handleArrowLeft(e: any) {
-    e.preventDefault();
-    setUsedButton(true);
-    if (currentSlide === 0) {
-      setCurrentSlide(4);
-    } else {
-      setCurrentSlide(currentSlide - 1);
-    }
-  }
-
-  function handleArrowRight(e: any) {
-    e.preventDefault();
-    setUsedButton(true);
-    console.log("botao usado?", usedButton);
-    if (currentSlide === 4) {
-      setCurrentSlide(0);
-    } else {
-      setCurrentSlide(currentSlide + 1);
-    }
-  }
-
-  function renderSlides(slides: any) {
-    return slides.map((slide: any) => (
-      <div className="box" key={slide.description} style={boxStyle}>
-        <img src={slide.image} />
-        <div className="info text-right p-8">
-          <div className="info-description">
-            <h5 className="title font-bold text-[26px]">{slide.title}</h5>
-            <p className="description text-[12px] font-light">
-              {slide.description}
-            </p>
-          </div>
+function renderSlides(slides: any) {
+  return slides.map((slide: any) => (
+    <div className="box" key={slide.key}>
+      <img src={slide.image} />
+      <div className="info text-right p-8">
+        <div className="info-description">
+          <h5 className="title font-bold text-[26px]">{slide.title}</h5>
+          <p className="description text-[12px] font-light">
+            {slide.description}
+          </p>
         </div>
       </div>
-    ));
-  }
-
-  return (
-    <div className="carousel">
-      <div className="arrows">
-        <button className="arrow_left" onClick={(e) => handleArrowLeft(e)}>
-          <FaAngleLeft />
-        </button>
-        <button className="arrow_right" onClick={(e) => handleArrowRight(e)}>
-          <FaAngleRight />
-        </button>
-      </div>
-      {renderSlides(slides)}
     </div>
-  );
+  ));
+}
+
+function Carousel() {
+  var settings = {
+    infinite: true,
+    arrows: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return <Slider {...settings}>{renderSlides(slides)}</Slider>;
 }
 
 export default Carousel;
