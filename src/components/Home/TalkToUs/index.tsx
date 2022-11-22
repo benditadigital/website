6f42691b7de9128d14f76480559299fc6bbead4d
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-22 15:21:41 -0300

    :bulb: Create TalkToUs

---
+++ src/components/Home/TalkToUs/index.tsx
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+function TalkToUs() {
+  return (
+    <div className="talk-to-us pl-10 font-poppins font-extralight text-white bg-black justify-center items-center">
+      <div className="text-left pr-5">
+        <h2 className="text-[64px] font-semibold">Fale Conosco</h2>
+      </div>
+      <div className="font-extralight text-[16x]">
+        Temos especialistas em desenvolvimento, design e marketing prontos
+        <br /> para atender a sua demanda, preencha o formulário e entramos em
+        contato o mais breve possível!
+      </div>
+    </div>
+  );
+}
+
+export default TalkToUs;
