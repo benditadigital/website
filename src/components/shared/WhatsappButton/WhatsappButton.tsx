022617e8f95c99de628807a0a5c297baeb99d71c
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-23 18:55:32 -0300

    :bulb: Create WhatsappButton

---
+++ src/components/shared/WhatsappButton/WhatsappButton.tsx
@@ -0,0 +1,11 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+import "./styles.css";
+
+export default function WhatsappButton() {
+  return (
+    <a className="whatsapp-button">
+      <FaWhatsapp size={40} />
+    </a>
+  );
+}
