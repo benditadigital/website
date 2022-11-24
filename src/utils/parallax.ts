697636b967eff4e55174ab517816e9f2e95369f0
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-24 13:11:19 -0300

    :bulb: Add utils

---
+++ src/utils/parallax.ts
@@ -0,0 +1,9 @@
+export function calculateTranslateY(percentage: any, multiplier = 1) {
+  if (percentage <= 1) {
+    var value = ((-35 * (1 - percentage) + 35 * percentage) * multiplier)
+      .toFixed(2)
+      .toString();
+    console.log(value);
+    return value + "px";
+  }
+}
