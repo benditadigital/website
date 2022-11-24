7370bc771552a989720fd1aa2c4f3634b593cf9d
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-24 15:12:49 -0300

    :arrow_up: Make Carousel Responsive

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -7,7 +7,6 @@ import carousel4 from "../../assets/img/carousel/4.jpg";
 import { useEffect, useState } from "react";
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
-import TinySlider from "tiny-slider-react";
 import "tiny-slider/dist/tiny-slider.css";
 
 import Slider from "react-slick";
@@ -67,24 +66,7 @@ function Carousel() {
     pauseOnHover: false,
     responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 640,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
