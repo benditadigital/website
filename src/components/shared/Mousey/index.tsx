7ac9d601d40ddb36f5b3a6c85916284f0c876586
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-11-24 13:15:29 -0300

    :bulb: Add animations

--- src/components/shared/Mousey/index.tsx
+++ src/components/shared/Mousey/index.tsx
@@ -1,10 +1,26 @@
 import "./styles.css";
+import { motion } from "framer-motion";
+import { InView } from "react-intersection-observer";
 
 function Mousey() {
   return (
-    <div className="mousey">
-      <div className="scroller"></div>
-    </div>
+    <InView threshold={0.25}>
+      {({ ref, inView }) => (
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, transform: "translateX(-40px)" }}
+          animate={
+            inView
+              ? { opacity: 1, transform: "translateX(0px)" }
+              : { opacity: 0 }
+          }
+          transition={{ duration: 1 }}
+          className="mousey"
+        >
+          <div className="scroller"></div>
+        </motion.div>
+      )}
+    </InView>
   );
 }
 
