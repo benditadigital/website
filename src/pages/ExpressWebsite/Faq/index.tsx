ff93aeb063c0ff815544be896062dfd7e3d88ec3
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-05 18:40:40 -0300

    :bulb: Add Faq component

---
+++ src/pages/ExpressWebsite/Faq/index.tsx
@@ -0,0 +1,76 @@
+import "./styles.css";
+import { Disclosure, Transition } from "@headlessui/react";
+import { HiPlusSm, HiMinusSm } from "react-icons/hi";
+import questions from "./questions";
+import ClassicButton from "../../../components/shared/ClassicButton";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+} from "react-html-parser";
+
+function renderQuestions(questions: Array<Object>, method: string) {
+  const half = Math.ceil(questions.length / 2);
+  var parsedQuestions = [];
+  if (method === "firstHalf") {
+    parsedQuestions = questions.slice(0, half);
+  } else {
+    parsedQuestions = questions.slice(half);
+  }
+
+  return parsedQuestions.map((question: Object) => {
+    return (
+      <div className="mb-1">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="disclosure-btn p-5 w-[100%] text-start font-medium bg-white disclosureButton flex items-center gap-2">
+                {open ? <HiMinusSm size={20} /> : <HiPlusSm size={20} />}
+                {question.title}
+              </Disclosure.Button>
+              <Transition
+                enter="transition ease duration-500 transform"
+                enterFrom="opacity-0 -translate-y-12"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease duration-300 transform"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 -translate-y-12"
+              >
+                <Disclosure.Panel className="disclosure-pnl text-black bg-[#F4F4F4] p-5 disclosurePanel">
+                  {ReactHtmlParser(question.description)}
+                </Disclosure.Panel>
+              </Transition>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    );
+  });
+}
+
+export default function Faq() {
+  return (
+    <section className="express-website faq pt-10">
+      <div className="container mx-auto max-w-[1180px]">
+        <div className="text-center py-[50px]">
+          <p className="font-normal text-[15px]">
+            Quem conhece indica! Gestão e Criação de Websites.
+          </p>
+          <h3 className="font-semibold text-[40px]">Clientes e Avaliações</h3>
+        </div>
+        <div className="grid grid-cols-2 gap-10 pb-[80px]">
+          <div>{renderQuestions(questions, "firstHalf")}</div>
+          <div>{renderQuestions(questions, "secondHalf")}</div>
+        </div>
+        <div className="text-center flex flex-col justify-center items-center pb-[80px]">
+          <span className="font-medium text-[20px]">
+            Ficou com alguma dúvida?
+          </span>
+          <ClassicButton
+            className="mt-5 variant-top-left"
+            title="Fale Conosco no Whatsapp"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/ExpressWebsite/Faq/questions.tsx
@@ -0,0 +1,83 @@
+export default [
+  {
+    title: `Meu site aparecerá no Google?`,
+    description: `Sim, está incluso em nosso trabalho de desenvolvimento de
+      sites cadastrar seu site no Google e otimizar ele de
+      acordo com as boas práticas de SEO.
+      <br />
+      <br />
+      Um site não fica na primeira página do Google se não
+      seguir alguns parâmetros impostos pelo próprio Google na
+      hora da criação. E nós estudamos e acompanhamos isso
+      diariamente para poder executar as melhores práticas após
+      criar seu site.`,
+  },
+  {
+    title: `Quais funcionalidades vocês oferecem?`,
+    description: `Nossos desenvolvedores trabalham
+      para entregar tudo que você precisa!
+      Nossas principais funcionalidades são Botão Whatsapp,
+      Chat online, Blog, Site multi-idioma,
+      Sistema de cursos online, Integração com e-mail marketing,
+      Pagamento digital, Área de membros e muitas outras funcionalidades
+      sob medida.`,
+  },
+  {
+    title: `Site One Page é bom para minha empresa?`,
+    description: `Em criação de websites,
+      o site one page é um tipo de layout feito
+      para exibir todo o conteúdo em apenas uma página,
+      com seções bem definidas e conectadas.
+      Por ser leve, dinâmico e funcional o site one page
+      é uma solução de desenvolvimento web muito moderna,
+      que vem sendo bastante procurada.
+      <br />
+      <br />
+      Você pode até adicionar outras páginas ao site,
+      mas o “núcleo”, isto é, as principais informações,
+      são exibidas em uma única página.
+      <br />
+      <br />
+      Por só ter uma página o site é muito rápido
+      e tem uma performance excelente, principalmente no mobile.`,
+  },
+  {
+    title: `Quanto tempo demora para se criar um site?`,
+    description: `O prazo varia muito de acordo com o tamanho e
+       complexidade do site. Em média os sites tendem a ser totalmente
+       desenvolvido em torno de 10 a 15 dias, porém pode ser em menos
+       tempo ou em mais tempo, dependendo também da cooperação do 
+       cliente e de outros fatores de implementação do projeto.`,
+  },
+  {
+    title: `Como fazer um orçamento para um site?`,
+    description: `Nossa dica para solicitar o orçamento de site é definir
+      o tipo de site que deseja e quais funcionalidades ou integrações serão
+      necessárias. Quanto mais detalhes relevantes você fornece,
+      mais assertivo será o nosso orçamento para sua demanda.`,
+  },
+  {
+    title: `Como é o processo de criação de um site?`,
+    description: `O processo para criar um site é simples,
+      porém tem que ser iniciado da maneira certa para ter um bom resultado.
+      De forma resumida temos 7 etapas:
+      <br />
+      <br />
+      Um site não fica na primeira página do Google se não
+      seguir alguns parâmetros impostos pelo próprio Google na
+      hora da criação. E nós estudamos e acompanhamos isso
+      diariamente para poder executar as melhores práticas após
+      criar seu site.
+      <br />
+      <br />
+      1. Ouvir o cliente + Briefing<br />
+      2. Conteúdo (Pesquisa de Mercado + SEO)<br />
+      3. Criação do layout<br />
+      4. Programação<br />
+      5. Testes e checklist<br />
+      6. Lançamento<br />
+      7. Manutenção<br />
+      <br />
+      A Bendita irá te auxiliar em todas as etapas!`,
+  },
+];
