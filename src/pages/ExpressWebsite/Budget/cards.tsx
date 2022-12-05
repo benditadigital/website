b425372f8dfaa9524ea1ac20646336e380b581ce
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-05 18:40:25 -0300

    :bulb: Add Budget component

---
+++ src/pages/ExpressWebsite/Budget/cards.tsx
@@ -0,0 +1,44 @@
+import icon1 from "./assets/img/document.png";
+import icon2 from "./assets/img/rocket.png";
+
+export default [
+  {
+    icon: icon1,
+    type: "BARATO",
+    title: "Site Expresso",
+    subtitle: "Site One Page Simples",
+    heading: `<p class="small-heading">À partir de</p>
+              <p class="large-heading"> R$ 500 </p>`,
+    traits: [
+      "Criação de site profissional simples",
+      "Entregue em até 5 dias úteis",
+      "Design responsivo (desktop, tablet, celular)",
+      "Personalização total do site",
+      "Todas as informações do site diagramadas em 1 página",
+      "Formulários e botões para gerar leads",
+      "Integração com redes sociais",
+      "Google friendly",
+      "Suporte e Manutenção",
+      "Conformidade com a LGPD",
+    ],
+  },
+  {
+    icon: icon2,
+    type: "EXCLUSIVO",
+    title: "Site Sob Medida",
+    subtitle: "Melhor Escolha",
+    heading: `<p class="large-heading">  Sob Orçamento </p>`,
+    traits: [
+      "Criação de site profissional com design único e exclusivo",
+      "Layout exclusivo sob medida",
+      "Design responsivo (desktop, tablet, celular)",
+      "Desenvolvimento com Prototipação",
+      "Institucional, E-commerce, Blogs e afins",
+      "Integrações customizadas",
+      "Otimização SEO para Buscas no Google​",
+      "Formulários e botões de para gerar leads​",
+      "Integração com redes sociais​",
+      "Conformidade com a LGPD",
+    ],
+  },
+];
---
+++ src/pages/ExpressWebsite/Budget/index.tsx
@@ -0,0 +1,88 @@
+import cards from "./cards";
+import "./styles.css";
+
+import { BsCheck } from "react-icons/bs";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+} from "react-html-parser";
+import ClassicButton from "../../../components/shared/ClassicButton";
+import TalkToUs from "../../../components/Home/TalkToUs";
+
+function renderCards(cards: Array<Object>) {
+  function renderTraits(traits: Array<string>) {
+    if (traits)
+      return traits.map((trait: string) => (
+        <div className="traits__trait font-light my-3">
+          <BsCheck size={30} color="#4a15bd" className="inline" />
+          {trait}
+        </div>
+      ));
+  }
+
+  if (cards)
+    return cards.map((card: Object) => {
+      return (
+        <div className="card text-white col-span-2 md:col-span-1">
+          <div className="card__classification">
+            <small>{card.type}</small>
+          </div>
+          <div className="card__header py-10 gap-2">
+            <div className="header__icon">
+              <img src={card.icon} />
+            </div>
+            <h6 className="header__title font-semibold text-[18px]">
+              {card.title}
+            </h6>
+            <p className="header__subtitle text-[16px] font-light">
+              {card.subtitle}
+            </p>
+          </div>
+          <div className="card__heading py-[35px] px-10">
+            {ReactHtmlParser(card.heading)}
+          </div>
+          <div className="card__traits px-10 my-[60px]">
+            {renderTraits(card.traits)}
+          </div>
+          <div className="card_footer text-center bg-[#efefef] py-10">
+            <a
+              className="bg-[var(--benditaPurple)]
+              font-bold text-white
+              hover:text-[var(--benditaPurple)]
+              hover:bg-white py-3 px-5
+              rounded-3xl"
+              href=""
+            >
+              Peça um Orçamento
+            </a>
+            <br />
+            <br />
+            <p className="text-[#9a9a9a]">A melhor escolha!</p>
+          </div>
+        </div>
+      );
+    });
+}
+
+export default function Budget() {
+  return (
+    <section className="budget">
+      <div className="container mx-auto max-w-[1180px] py-[50px] px-10">
+        <p className="text-[15px] pt-[25px] text-white text-center">
+          Preços e Orçamentos
+        </p>
+        <h4 className="text-[45px] pt-[5px] pb-[55px] text-white text-center font-semibold">
+          Contrate um Site Profissional!
+        </h4>
+        <div className="grid grid-cols-2 gap-10">{renderCards(cards)}</div>
+        <div className="pt-[50px] pb-[100px] text-center text-white">
+          <p>E aí, vamo fechar? 😉</p>
+          <ClassicButton
+            className="variant-top-left mt-6"
+            title="Fale Conosco no WhatsApp!"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
