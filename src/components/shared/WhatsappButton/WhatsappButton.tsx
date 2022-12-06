095497cc1b6d99b32a58d70d565f8e5da2311f3c
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-06 14:28:04 -0300

    :wrench: Fixed href

--- src/components/shared/WhatsappButton/WhatsappButton.tsx
+++ src/components/shared/WhatsappButton/WhatsappButton.tsx
@@ -4,7 +4,11 @@ import "./styles.css";
 
 export default function WhatsappButton() {
   return (
-    <a className="whatsapp-button">
+    <a
+      className="whatsapp-button"
+      href="https://wa.me/+5561981466888"
+      target="_blank"
+    >
       <FaWhatsapp size={40} />
     </a>
   );
