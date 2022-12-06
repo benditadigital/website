410f863cbd4a2e139c89b844b7dd5e60a687ea33
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-06 14:24:17 -0300

    :arrow_up: Update Hero component

--- src/pages/_sharedComponents/Hero/index.tsx
+++ src/pages/_sharedComponents/Hero/index.tsx
@@ -15,19 +15,23 @@ export default function Hero(props: HeroProps) {
     <section className="text-white express-website-hero font-poppins">
       <div className="container py-[150px] min-h-[600px] max-w-[1180px] mx-auto">
         <div className="grid grid-cols-2 flex justify-center items-center">
-          <div>
+          <div className="order-0 col-span-2 md:col-span-1 px-8 md:px-0">
             <h5 className="text-[15px]">👋 {props.pretitle}</h5>
             <h3 className="text-[55px] font-semibold leading-none">
               {props.title}
             </h3>
             <p className="font-light text-[16px] my-8">{props.description}</p>
-            <ClassicButton
-              title="Solicite um Orçamento"
-              icon={<RiArrowRightSLine size={23} />}
-              className="variant-bottom-left"
-            />
+            <div className="text-center md:text-left">
+              <div className="flex">
+                <ClassicButton
+                  title="Solicite um Orçamento"
+                  icon={<RiArrowRightSLine size={23} />}
+                  className="variant-bottom-left"
+                />
+              </div>
+            </div>
           </div>
-          <div className="flex justify-center">
+          <div className="hidden md:flex justify-center">
             <img
               src={alternativaLogo}
               height={"100%"}
