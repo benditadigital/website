8541d168b7fb4092a24a957ade04866432980891
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-06 14:27:48 -0300

    :wrench: Fixed href

--- src/components/shared/ClassicButton/index.tsx
+++ src/components/shared/ClassicButton/index.tsx
@@ -8,7 +8,9 @@ interface ClassicButtonProps {
 
 export default function ClassicButton(props: ClassicButtonProps) {
   return (
-    <button
+    <a
+      href="https://wa.me/+5561981466888"
+      target="_blank"
       className={
         "classic-button font-poppins font-medium py-3 px-3 pr-2 " +
         props.className
@@ -17,6 +19,6 @@ export default function ClassicButton(props: ClassicButtonProps) {
       <span className="mx-3 flex justify-center items-center gap-2">
         {props.title} {props.icon}
       </span>
-    </button>
+    </a>
   );
 }
