6d2abd72739cdc43a64408e2b7d5291f944018e6
José Guilherme <joseguilhermefmoura@gmail.com>
2022-12-06 14:27:56 -0300

    :wrench: Fixed href

--- src/components/shared/NeonButton/index.tsx
+++ src/components/shared/NeonButton/index.tsx
@@ -8,18 +8,19 @@ interface NeonButtonProps {
 
 function NeonButton(props: NeonButtonProps) {
   return (
-    <button
+    <a
       className={"neon-button font-poppins font-light px-5 " + props.className}
+      href="https://wa.me/+5561981466888"
+      target="_blank"
     >
       <span></span>
       <span></span>
       <span></span>
       <span></span>
       <span className="text-white mx-3 flex justify-center items-center gap-3">
-        {" "}
         {props.title} {props.icon}
       </span>
-    </button>
+    </a>
   );
 }
 
